4ad05453e2891cfcdd655a02bada4411c39bfe91
Naresh <nbk.mfadvisor@gmail.com>
2025-12-10 17:16:17 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,13 +1,10 @@
-# This file is edited in remote repo
+# 1.This file is edited in remote repo
+# 2.Updated from remote repo
     
     public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git from Java!");
     }
 }
-
-
-
-
 
 # Again Commiting from local repo
